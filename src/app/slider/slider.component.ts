6ecf690cfa36545e1e9b3a3367759882da8aca82
NatashaMelnik <n.melnik1788@gmail.com>
2021-05-03 13:45:37 +0430

    test drag

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -6,9 +6,7 @@ import { SliderDatasService } from '../services/slider-datas.service';
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss'],
 })
-
 export class SliderComponent implements OnInit {
-
   users;
   images;
   elements;
@@ -21,27 +19,31 @@ export class SliderComponent implements OnInit {
     width: 131,
     position: 0,
     count: 1,
-  }
+  };
 
   optionIm = {
     width: 131,
     position: 0,
     count: 1,
-  }
+  };
 
   optionCom = {
     width: 131,
     position: 0,
     count: 1,
-  }
+  };
 
-  @ViewChild("list", {read: ElementRef}) list: ElementRef;
-  @ViewChild("listIm", {read: ElementRef}) listIm: ElementRef;
-  @ViewChild("listEl", {read: ElementRef}) listEl: ElementRef;
+  @ViewChild('list', { read: ElementRef }) list: ElementRef;
+  @ViewChild('listIm', { read: ElementRef }) listIm: ElementRef;
+  @ViewChild('listEl', { read: ElementRef }) listEl: ElementRef;
 
   ngOnInit(): void {}
 
-  constructor(_useList: SliderDatasService, _useImList: SliderDatasService, _useCmList: SliderDatasService) {
+  constructor(
+    _useList: SliderDatasService,
+    _useImList: SliderDatasService,
+    _useCmList: SliderDatasService
+  ) {
     this.listName = _useList.getListNames();
     this.users = this.listName;
 
@@ -56,26 +58,25 @@ export class SliderComponent implements OnInit {
     this.clickPrev(this.optionIm, this.listIm);
   }
 
-  imageSliderNext(): void{
+  imageSliderNext(): void {
     this.clickNext(this.optionIm, this.listIm);
   }
 
-  textSliderPrev(): void{
+  textSliderPrev(): void {
     this.clickPrev(this.optionTx, this.list);
   }
 
-  textSliderNext(): void{
+  textSliderNext(): void {
     this.clickNext(this.optionTx, this.list);
   }
 
-  combSliderPrev(): void{
+  combSliderPrev(): void {
     this.clickPrev(this.optionCom, this.listEl);
   }
 
-  combSliderNext(): void{
+  combSliderNext(): void {
     this.clickNext(this.optionCom, this.listEl);
   }
-  
 
   clickPrev(option, list): void {
     option.position += option.width * option.count;
@@ -85,8 +86,73 @@ export class SliderComponent implements OnInit {
 
   clickNext(option, list): void {
     option.position -= option.width * option.count;
-    option.position = Math.max(option.position, -option.width * (this.users.length - option.count));
+    option.position = Math.max(
+      option.position,
+      -option.width * (this.users.length - option.count)
+    );
     list.nativeElement.style.marginLeft = option.position + 'px';
   }
 
+  // ______swipe______
+
+  slider_1 = {
+    cond1_1: false,
+    cond2_1: false,
+    mousedownFired_1: false,
+    index_1: 0,
+    xDown_1: 0,
+  };
+
+  slider_2 = {
+    cond1_2: false,
+    cond2_2: false,
+    mousedownFired_2: false,
+    index_2: 0,
+    xDown_2: 0,
+  };
+
+  slider_3 = {
+    cond1_3: false,
+    cond2_3: false,
+    mousedownFired_3: false,
+    index_3: 0,
+    xDown_3: 0,
+  };
+
+  dragover(num: number) {
+    console.log('dragover');
+    this.textSliderNext();
+    // if (num === 0) {this.textSliderNext()};
+    // if (num === 1) {this.imageSliderNext()};
+    // if (num === 2) {this.combSliderNext()};
+  }
+
+  // $('.carousel').mousedown(function () {
+  //   cond1 = true;
+  //   xDown = event.clientX;
+  //   mousedownFired = true;
+  // });
+
+  // $('.carousel').mousemove(function () {
+  //   this.cond2 = true;
+  // });
+
+  // $('.carousel').mouseup(function () {            // for swipe
+  //   event.stopPropagation();
+  //   var xUp = event.clientX;
+  //   if (this.cond1 && this.cond2){
+  //     swipe(xUp);
+  //   }
+  //   cond1 = false;
+  //   cond2 = false;
+  // });
+
+  // function swipe(xUp) {
+  //   if (xDown > xUp) {
+  //     rightClick();
+  //   }
+  //   else {
+  //     leftClick();
+  //   }
+  // }
 }
